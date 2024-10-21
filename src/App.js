27f96a205d2c39995e9bc8a8011b206911ecae68
gowtham144 <garepall@gitam.in>
2024-10-21 14:50:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import MapComponent from './Components/Mapcomponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Land Parcel Management System</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Mapcomponent.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, DrawingManager } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '500px',
+};
+
+const center = {
+    lat: 17.5497769,
+    lng: 78.1548201, 
+  };
+  
+  
+
+function MapComponent() {
+  const [mapLoaded, setMapLoaded] = useState(false); // Track if the map is loaded
+
+  const handleOnLoad = () => {
+    setMapLoaded(true);
+  };
+
+  const handleOnPolygonComplete = (polygon) => {
+    const path = polygon.getPath();
+    const coordinates = [];
+    for (let i = 0; i < path.getLength(); i++) {
+      coordinates.push({
+        lat: path.getAt(i).lat(),
+        lng: path.getAt(i).lng(),
+      });
+    }
+    console.log('Polygon Coordinates:', coordinates);
+  };
+
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyA_Z1_l75AWkfTNqvjX5Sp0wMejwP33ZTo"
+      libraries={['drawing']}
+      onLoad={handleOnLoad}
+    >
+      {mapLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          options={{
+            mapTypeId: 'satellite',
+            disableDefaultUI: true,
+          }}
+        >
+          <DrawingManager
+            onPolygonComplete={handleOnPolygonComplete}
+            options={{
+              drawingControl: true,
+              drawingControlOptions: {
+                position: window.google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: ['polygon'],
+              },
+              polygonOptions: {
+                fillColor: '#000',
+                fillOpacity: 0.5,
+                strokeWeight: 1,
+                clickable: true,
+                editable: true,
+                draggable: true,
+              },
+            }}
+          />
+        </GoogleMap>
+      )}
+    </LoadScript>
+  );
+}
+
+export default MapComponent;
